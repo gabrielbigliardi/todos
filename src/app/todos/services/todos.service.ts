fc220b0484b14010f51e0ad85bae52986bdb7554
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2024-04-23 17:55:53 -0300

    addTodo

---
+++ src/app/todos/services/todos.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, signal } from "@angular/core";
+import { TodoInterface } from "../types/todo.interface";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TodosService {
+    todosSig = signal<TodoInterface[]>([])
+
+    addTodo(text: string): void {
+        const newTodo: TodoInterface = {
+            text,
+            isCompleted: false,
+            id: Math.random().toString(16),
+        }
+    }
+}
